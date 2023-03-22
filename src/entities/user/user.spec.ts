5a6fc9d8334e98bfa75d6a4ff530206e30bf07af
João Bispo <joaobispo2077@gmail.com>
2023-03-22 11:31:42 -0300

    test: verify name while creating user

--- src/entities/user/user.spec.ts
+++ src/entities/user/user.spec.ts
@@ -1,6 +1,7 @@
 import { User } from '.';
 import { left } from '../../shared/either';
 import { InvalidEmailError } from '../errors/invalid-email-error';
+import { InvalidNameError } from '../errors/invalid-name-error';
 
 describe('User domain', () => {
   it('should not create user with invalid e-mail address', () => {
@@ -9,5 +10,25 @@ describe('User domain', () => {
     const error = User.create({ name: 'any_name', email: invalidEmail });
 
     expect(error).toEqual(left(new InvalidEmailError()));
+  });
+
+  it('should not create user with invalid name (too few chars)', () => {
+    const invalidName = 'O          ';
+    const error = User.create({
+      name: invalidName,
+      email: 'john.doe@gmail.com',
+    });
+
+    expect(error).toEqual(left(new InvalidNameError()));
+  });
+
+  it('should not create user with invalid name (too many chars)', () => {
+    const invalidName = 'O'.repeat(256);
+    const error = User.create({
+      name: invalidName,
+      email: 'john.doe@gmail.com',
+    });
+
+    expect(error).toEqual(left(new InvalidNameError()));
   });
 });
