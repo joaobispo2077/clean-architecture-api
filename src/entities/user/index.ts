bb5d6aade9e0c9b8041c54c4cb82e26e27383c26
João Bispo <joaobispo2077@gmail.com>
2023-03-22 11:17:03 -0300

    feat: verify e-mail address when creating a user

---
+++ src/entities/user/index.ts
@@ -0,0 +1,16 @@
+import { Either, left, right } from '../../shared/either';
+import { Email } from '../email';
+import { InvalidEmailError } from '../errors/invalid-email-error';
+import { UserData } from '../user-data';
+
+export class User {
+  static create(userData: UserData): Either<InvalidEmailError, User> {
+    const emailOrError = Email.create(userData.email);
+
+    if (emailOrError.isLeft()) {
+      return left(new InvalidEmailError());
+    }
+
+    return right({});
+  }
+}
