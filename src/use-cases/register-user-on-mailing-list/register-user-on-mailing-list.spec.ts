4b209f458841d4f6c834c2b769d264e97c6268da
João Bispo <joaobispo2077@gmail.com>
2023-03-23 20:33:58 -0300

    test: register valid user to mailing list

--- src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,18 +1,22 @@
-// import { UserData } from './user-data';
+import { UserData } from '../../entities/user-data';
+import { UserRepository } from './ports/user-repository';
+import { RegisterUserOnMailingListUseCase } from './register-user-on-mailing-list';
+import { InMemoryUserRepository } from './repository/in-memory-user-repository';
 
 describe('[UseCase] - Register user on mailing list', () => {
-  it('should add user with compete data to mailing list', async () => {
-    // const users: UserData[] = [];
-    // const userRepository: UserRepository = new InMemoryUserRepository(users);
-    // const useCaseRegisterUseOnMailingList: RegisterUserOnMailingListUseCase =
-    //   new RegisterUserOnMailingListUseCase(userRepository);
-    // const name = 'John Doe';
-    // const email = 'john.doe@gmail.com';
-    // const response = await useCaseRegisterUseOnMailingList.execute({
-    //   name,
-    //   email,
-    // });
-    // const user = await userRepository.findByEmail(email);
-    // expect(user.name).toBe(name);
+  it('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = [];
+    const userRepository: UserRepository = new InMemoryUserRepository(users);
+    const useCaseRegisterUseOnMailingList: RegisterUserOnMailingListUseCase =
+      new RegisterUserOnMailingListUseCase(userRepository);
+    const name = 'John Doe';
+    const email = 'john.doe@gmail.com';
+    const response = await useCaseRegisterUseOnMailingList.execute({
+      name,
+      email,
+    });
+    const user = await userRepository.findUserByEmail(email);
+    expect(user?.name).toBe(name);
+    expect(response.value.name).toBe(name);
   });
 });
