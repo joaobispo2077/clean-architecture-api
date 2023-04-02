d5ce8d448d6364d31a1d7f25b6415a92791d870f
João Bispo <joaobispo2077@gmail.com>
2023-04-02 14:17:17 -0300

    test: apply cors middleware

---
+++ tests/main/configs/middlewares/cors.test.ts
@@ -0,0 +1,17 @@
+import { app } from '@src/main/configs/app';
+import request from 'supertest';
+
+describe('Cors middleware', () => {
+  it('should enable CORS', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send();
+    });
+
+    await request(app)
+      .get('/test_cors')
+      .send({ name: 'John Doe' })
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-headers', '*')
+      .expect('access-control-allow-methods', '*');
+  });
+});
