27c125ff3582582fd4d6539321a71e3830bf2b65
João Bispo <joaobispo2077@gmail.com>
2023-04-02 13:39:27 -0300

    feat: add use case interface

---
+++ src/use-cases/ports/use-case.ts
@@ -0,0 +1,3 @@
+export interface UseCase {
+  execute(request: any): Promise<any>;
+}
--- src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.ts
+++ src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -2,8 +2,9 @@ import { InvalidEmailError, InvalidNameError } from '@src/entities/errors';
 import { User, UserData } from '@src/entities';
 import { Either, left, right } from '@src/shared';
 import { UserRepository } from './ports';
+import { UseCase } from '../ports';
 
-export class RegisterUserOnMailingListUseCase {
+export class RegisterUserOnMailingListUseCase implements UseCase {
   constructor(private readonly userRepository: UserRepository) {}
 
   async execute(
