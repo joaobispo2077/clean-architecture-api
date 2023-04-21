bd36ca602173e38f9f6cc04ff9213c0f119937a3
João Bispo <joaobispo2077@gmail.com>
2023-04-21 15:38:43 -0300

    feat: add prefix /api into router

--- src/main/configs/router.ts
+++ src/main/configs/router.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
 export const setupRoutes = (app: Express) => {
   const router = Router();
 
-  app.use('/api');
+  app.use('/api', router);
 
   // read directories names and import all routes to activate routing
   readdirSync(join(__dirname, 'routes')).forEach(async (file) => {
