06a0cc5dd2f50a4f06129aff35c4688ec446f322
João Bispo <joaobispo2077@gmail.com>
2023-04-24 11:58:42 -0300

    fix: adjust path import of in memory user repository

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,6 +1,6 @@
 import { RegisterUserController } from '@src/controllers';
 import { RegisterUserOnMailingListUseCase } from '@src/use-cases/register-user-on-mailing-list';
-import { InMemoryUserRepository } from '@tests/use-cases/register-user-on-mailing-list/repository/in-memory-user-repository';
+import { InMemoryUserRepository } from '@src/use-cases/repositories/in-memory-user-repository';
 
 export const makeRegisterUserController = (): RegisterUserController => {
   const userRepository = new InMemoryUserRepository([]);
