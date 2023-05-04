2827795789d1c2b07cb47f9b5ec9cea5d60ab818
João Bispo <joaobispo2077@gmail.com>
2023-05-04 20:28:17 -0300

    chore: add PORT as environment variable

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,6 @@
 import 'module-alias/register';
 import { app } from './configs/app';
 
-const port = 3333;
+const port = Number(process.env.PORT ?? 3333);
 
 app.listen(port, () => console.info(`Server is running at por ${port}`));
