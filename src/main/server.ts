deb3f420d04023eb6aa3c91aa7b048074acb32a1
João Bispo <joaobispo2077@gmail.com>
2023-05-06 15:13:19 -0300

    feat: add mongodb connection when start app

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,14 @@
 import 'module-alias/register';
-import { app } from './configs/app';
+import { MongoDbHelper } from '@src/infra/repositories/helpers';
 
-const port = Number(process.env.PORT ?? 3333);
+MongoDbHelper.connect(String(process.env.MONGO_URL))
+  .then(async () => {
+    const module = await import('./configs/app');
 
-app.listen(port, () => console.info(`Server is running at por ${port}`));
+    const { app } = module;
+
+    const port = Number(process.env.PORT ?? 3333);
+
+    app.listen(port, () => console.info(`Server is running at por ${port}`));
+  })
+  .catch((err) => console.error(err));
